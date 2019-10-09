9359d1b6d843ce9f3e51e4c9759c055948246930
enPiT-P10 <d913@oshima-k.ac.jp>
2019-10-09 14:34:49 +0900

    firebaseと接続した

--- app/src/main/java/com/example/sample/MainActivity.kt
+++ app/src/main/java/com/example/sample/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.example.sample
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val key = "color"
+        val value = "blue"
+
+        val database = FirebaseDatabase.getInstance()
+        val ref = database.getReference(key)
+        ref.setValue(value)
+
+        ref.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val value = dataSnapshot.value
+                textView.text = value.toString()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w("onCancelled", "error:", error.toException())
+            }
+        })
     }
 }
